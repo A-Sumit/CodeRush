7457b2eb02505bebdde8480ee74f6a96e7255a9b
praveshkumar1 <pravesh10062003@gmail.com>
2024-08-10 15:16:19 +0530

    added cpp execution with file and input

--- backend/app.mjs
+++ backend/app.mjs
@@ -1,6 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { python } from 'compile-run';
+import { cpp } from 'compile-run';
+import { performance } from 'perf_hooks';
+import { memoryUsage } from 'process';
 const app = express();
 
 app.use(bodyParser.json());
@@ -9,23 +11,27 @@ app.get('/test', (req, res) => {
     res.send('hey working')
 });
 
-async function runPythonCode(sourceCode) {
-    try {
-        let result = await python.runSource(sourceCode);
-        return result;
-    } catch (error) {
-        throw error;
-    }
-}
-
 app.post('/run', async (req, res) => {
-        const { sourceCode } = req.body;
-        try {
-            const result = await runPythonCode(sourceCode);
-            res.json({ output: result.stdout, error: result.stderr });
-        } catch (err) {
-            res.status(500).json({ error: err.message });
-        }
+    const { filePath, input } = req.body;
+
+    if (!filePath || !input) {
+        return res.status(400).json({ error: 'filePath and input are required' });
+    }
+
+    try {
+        const startTime = performance.now();
+        const result = await cpp.runFile(filePath, { stdin: input });
+        const endTime = performance.now();
+        const executionTime = endTime - startTime;
+        res.json({
+            result:{
+                result
+            },
+            executionTime: `${executionTime.toFixed(2)} ms`,
+        });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 });
 app.listen(3000, () => {
     console.log('Server running on port 3000');
