d02286936c10cd6c83fe09a18aec6d2c8a00c5ff
A-Sumit <sumittheofficial@gmail.com>
2024-08-10 16:17:13 +0530

    more refactoring

--- backend/src/controllers/codeController.mjs
+++ backend/src/controllers/codeController.mjs
@@ -1,9 +1,4 @@
-import { cpp } from 'compile-run';
-import { performance } from 'perf_hooks';
-import { memoryUsage } from 'process';
-import { promises as fs } from 'fs';
-import path from 'path';
-import os from 'os';
+import {executeCppCode} from '../services/codeService.mjs';
 
 export const executeCode = async (req, res) => {
     const { filePath, sourceCode, input } = req.body;
@@ -16,30 +11,11 @@ export const executeCode = async (req, res) => {
         return res.status(400).json({ error: 'Input is required' });
     }
 
-    let tempFilePath;
     try {
-        if (sourceCode) {
-            const tempDir = os.tmpdir();
-            tempFilePath = path.join(tempDir, `temp-${Date.now()}.cpp`);
-            await fs.writeFile(tempFilePath, sourceCode);
-        }
-
-        const startTime = performance.now();
-        const result = await cpp.runFile(filePath || tempFilePath, { stdin: input });
-        const endTime = performance.now();
-        const executionTime = endTime - startTime;
-
-        res.json({
-            result: result,
-            executionTime: `${executionTime.toFixed(2)} ms`,
-        });
+        const result = await executeCppCode(filePath, sourceCode, input);
+        res.json(result);
     } catch (err) {
         res.status(500).json({ error: err.message });
-    } finally {
-        if (tempFilePath) {
-            // Clean up the temporary file
-            await fs.unlink(tempFilePath);
-        }
     }
 };
 
